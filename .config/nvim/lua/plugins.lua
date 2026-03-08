21c621146987a14488f60c4fc297b88eaaeb57ea
Arham Garg <arhamvgarg@gmail.com>
2026-03-08 19:29:40 +0530

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,7 +1,7 @@
-require("bufferline").setup({})
+require("bufferline").setup {}
 
-require("code_runner").setup({})
+require("code_runner").setup {}
 
-require("nvim-autopairs").setup({})
+require("nvim-autopairs").setup {}
 
-require("trouble").setup({})
+require("trouble").setup {}
