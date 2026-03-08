b7539558ca1ade311c6fe8cd297d27e6693bcb57
Arham Garg <arhamvgarg@gmail.com>
2026-03-08 19:50:38 +0530

    Update servers.lua

--- .config/nvim/lua/servers.lua
+++ .config/nvim/lua/servers.lua
@@ -2,19 +2,19 @@ vim.lsp.enable "clangd"
 
 vim.lsp.enable "lua_ls"
 
-vim.lsp.enable "pyright"
-
-vim.lsp.config("pyright", {
-  settings = {
-    pyright = {
-      disableOrganizeImports = true,
-    },
-    python = {
-      analysis = {
-        ignore = { "*" },
-      },
-    },
-  },
-})
+-- vim.lsp.enable("pyright")
+--
+-- vim.lsp.config("pyright", {
+--   settings = {
+--     pyright = {
+--       disableOrganizeImports = true,
+--     },
+--     python = {
+--       analysis = {
+--         ignore = { "*" },
+--       },
+--     },
+--   },
+-- })
 
 vim.lsp.enable "ruff"
