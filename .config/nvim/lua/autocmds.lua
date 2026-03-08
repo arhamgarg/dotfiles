445859c5b357dd173c42a042eb594a4e65045246
Arham Garg <arhamvgarg@gmail.com>
2026-03-08 19:24:05 +0530

    Update autocmds.lua

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,20 +1,20 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   callback = function()
-    vim.cmd([[
+    vim.cmd [[
       %s/\s\+$//e
       %s/\n\+\%$//e
-    ]])
+    ]]
   end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.py",
   callback = function()
-    vim.lsp.buf.code_action({
+    vim.lsp.buf.code_action {
       context = { only = { "source.organizeImports" } },
       apply = true,
-    })
+    }
     vim.wait(100)
   end,
 })
