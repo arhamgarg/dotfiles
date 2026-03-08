c7220d5cebca6c5bf0a224770661f6c7008aaad3
Arham Garg <arhamvgarg@gmail.com>
2026-03-08 19:50:21 +0530

    Update keymaps.lua

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -7,6 +7,8 @@ local function map(m, k, v, opts)
   vim.keymap.set(m, k, v, opts)
 end
 
+map("i", "jj", "<esc>")
+
 map("n", ";", ":")
 map("n", "<CR>", "o<esc>")
 map("n", "<S-CR>", "O<esc>")
@@ -20,19 +22,17 @@ map("n", "<C-j>", "<C-w>j")
 map("n", "<C-k>", "<C-w>k")
 map("n", "<C-l>", "<C-w>l")
 
+map("n", "<leader>x", "<cmd>w<CR><cmd>RunCode<CR><C-l>")
+map("n", "<leader>q", "<cmd>RunClose<CR>")
+
 local builtin = require "telescope.builtin"
 map("n", "<leader>ff", builtin.find_files)
 map("n", "<leader>fg", builtin.live_grep)
 map("n", "<leader>fb", builtin.buffers)
 map("n", "<leader>fh", builtin.help_tags)
 
-map("n", "<leader>x", "<cmd>w<CR><cmd>RunCode<CR><C-l>")
-map("n", "<leader>q", "<cmd>RunClose<CR>")
-
 map("n", "<leader>t", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>")
 
 map("v", ";", ":")
 map("v", "<", "<gv")
 map("v", ">", ">gv")
-
-map("i", "jj", "<esc>")
